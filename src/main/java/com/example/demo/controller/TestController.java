c132af6072e84d8084229e4fcd4fdf3d290c4637
Super <1330339392@qq.com>
2021-03-05 15:41:35 +0800

    init project

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,10 +1,16 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @GetMapping("add/{num1}/{num2}")
+    public int add(@PathVariable("num1")Integer a, @PathVariable("num2")Integer b){
+        return a+b;
+    }
 
     @GetMapping("/test")
     public String testMethod(){
